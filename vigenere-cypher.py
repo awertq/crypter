06d74f3483aad012f088fe36ff9edb2673740e8f
awertq <awert2684@gmail.com>
2019-10-07 01:56:15 +0530

    DictCSV on process

--- vigenere-cypher.py
+++ vigenere-cypher.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import lst_cretr as ls
+import csv
 
 #creating alphabet lists.
 # a==97 , z==122
@@ -47,7 +48,7 @@ alpha_arr= np.array([l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,
 #ENCODER
 
 key = input("Enter the encoding key   -->  ")
-test_lst = ['hello','bell'] #test
+test_lst = ['hello','bell','asdfgg'] #test
 #function to add seperated alphas.
 def jnr(lst):
 	for s in range(len(lst)):
@@ -58,14 +59,6 @@ def jnr(lst):
 			word_var +=temp_alpha
 		lst[s] = word_var
 
-
-'''#function to remove \n and substitute , . ! [{()}]
-def subs(word):
-	lst = list(word)
-	for z in range(len(lst)):
-		if lst[z] == '\n':
-			del lst[z]
-	return jnr(lst)'''
 
 #function to convert list element to alphalist the 
 
@@ -133,12 +126,172 @@ def encoder(snt_lst,key):
 encoder(test_lst,key) #test
 print(test_lst)  #test
 
-'''test'''
-##f_path = input('Enter file path   -->  ')
 
-'''with open('file.txt','r') as file :
-	data = file.readlines()
-	encoder(data,key)
-	with open('/home/awert/Desktop/crypter/file2.txt','w') as bub:
-		bub.writeline(data)
+
+#function to take a sentence and break it into list of words
+#making csv file contaning indexes of ,.!;:'"[{()}]?@
+
+sent = 'inni mini m.ino, ()(({{}}}:;''"""".....,,,,,,,,,,@@@@@!!11??????mo' #test
+
+comma = 0
+period = 0
+excl_mark = 0
+semi_col = 0
+col = 0
+appost = 0
+doub_appost = 0
+sq_o = 0
+cur_o = 0
+sml_o = 0
+sml_c = 0
+cur_c = 0
+sq_c = 0
+q_mark = 0
+at_rt = 0
+
+
+#making data csv from file to encrypt containing indexes of symbol.
+
+main_file = 'file.txt' #test
+def csv_mkr(main_file):
+	with open('symbol.csv','w') as csv_file:
+		csv_file.write('line-num~index~comma~period~excl_mark~semi_col~col~appost~doub_appost~sq_o~cur_o~sml_o~sml_c~cur_c~sq_c~q_mark~at_rt\n')
+
+	with open(main_file,'r') as file:
+		snt_ori = file.readlines()
+		for b in range(len(snt_ori)):
+			snt = str(snt_ori[b])
+			print(snt) #test
+
+			global comma
+			global period
+			global excl_mark
+			global semi_col
+			global col
+			global appost
+			global doub_appost
+			global sq_o
+			global cur_o
+			global sml_o
+			global sml_c
+			global cur_c
+			global sq_c
+			global q_mark
+			global at_rt
+
+			comma = 0
+			period = 0
+			excl_mark = 0
+			semi_col = 0
+			col = 0
+			appost = 0
+			doub_appost = 0
+			sq_o = 0
+			cur_o = 0
+			sml_o = 0
+			sml_c = 0
+			cur_c = 0
+			sq_c = 0
+			q_mark = 0
+			at_rt = 0
+
+			for y in range(len(snt)):
+
+				if snt[y]==',':
+					comma = 1
+				if snt[y]!=',':
+					comma = 0
+				if snt[y]=='.':
+					period = 1
+				if snt[y]!='.':
+					period = 0
+				if snt[y]=='!':
+					excl_mark = 1
+				if snt[y]!='!':
+					excl_mark = 0
+				if snt[y]==';':
+					semi_col = 1
+				if snt[y]!=';':
+					semi_col = 0
+				if snt[y]==':':
+					col = 1
+				if snt[y]!=':':
+					col = 0
+				if snt[y]=="'":
+					appost = 1
+				if snt[y]!="'":
+					appost = 0
+				if snt[y]=='"':
+					 doub_appost= 1
+				if snt[y]!='"':
+					 doub_appost= 0
+				if snt[y]=='[':
+					sq_o = 1
+				if snt[y]!='[':
+					sq_o = 0
+				if snt[y]=='{':
+					cur_o = 1
+				if snt[y]!='{':
+					cur_o = 0
+				if snt[y]=='(':
+					sml_o = 1
+				if snt[y]!='(':
+					sml_o = 0
+				if snt[y]==')':
+					sml_c = 1
+				if snt[y]!=')':
+					sml_c = 0
+				if snt[y]!='}':
+					cur_c = 1
+				if snt[y]!='}':
+					cur_c = 0
+				if snt[y]==']':
+					sq_c = 1
+				if snt[y]!=']':
+					sq_c = 0
+				if snt[y]=='?':
+					q_mark = 1
+				if snt[y]!='?':
+					q_mark = 0
+				if snt[y]=='@':
+					at_rt = 1
+				if snt[y]!='@':
+					at_rt = 0
+
+				with open('symbol.csv','a') as csv_file:
+
+					name_field = ['line-num','index','comma','period','excl_mark','semi_col','col,appost','doub_appost','sq_o','cur_o','sml_o','sml_c','cur_c','sq_c','q_mark','at_rt']
+					val_field = [b,y,comma,period,excl_mark,semi_col,col,appost,doub_appost,sq_o,cur_o,sml_o,sml_c,cur_c,sq_c,q_mark,at_rt]
+					for q in range(len(name_field)):
+						if name_field[q] == 'at_rt':
+							csv_file.write(str(val_field[q])+'\n')
+						else:
+							csv_file.write(str(val_field[q])+'~')
+
+
+
+''' To Add ->
+	#making dictCSV for easy reading and extracting data.
+	with open('symbol.csv','r') as file113:
+		csv_reader = csv.DictReader(file113)
+		with open('sample.csv','w') as file12:
+#			fields = ['line-num~index~comma~period~excl_mark~semi_col~col~appost~doub_appost~sq_o~cur_o~sml_o~sml_c~cur_c~sq_c~q_mark~at_rt']
+
+			fields = ['line-num','index','comma','period','excl_mark','semi_col','col,appost','doub_appost','sq_o','cur_o','sml_o','sml_c','cur_c','sq_c','q_mark','at_rt']
+			csv_writer = csv.DictWriter(file12,fieldnames=fields,delimiter='~')
+			for l112 in csv_reader:
+				csv_writer.writerow(l112)
 '''
+
+
+csv_mkr('file.txt') #test
+
+
+
+
+
+
+
+
+#def w_seperator(snt):
+##	snt.split(
